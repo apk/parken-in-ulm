fb140896c607ecee1fd5c60185fd1dfec09c9baa
Andreas Krey <a.krey@iocl.org>
2012-04-30 16:08:35 +0200

    put graphs of whole week on separate page, for faster mobile loading

--- park.sh
+++ park.sh
@@ -5,9 +5,9 @@ export LANG
 exec >>park.log 2>&1
 /usr/bin/ruby pick.rb
 
-n=0
 dir=../httproot/default/parken
-touch $dir/index.new
+
+n=0
 (for i in `cat data/flist`; do
 	ln -f $i.png $dir/day-$n.png
 	ln -f $i-n.png $dir/day-$n-s.png
@@ -16,7 +16,7 @@ touch $dir/index.new
 	d="~ `TZ=GMT date -r $i.png +'%Y-%m-%d'`"
 	case "$n" in
 	0)
-		echo '<p>('"$d"') Heute:'
+		echo '<p>('"$d"') (<a href="index.html">Nur</a>) Heute:'
 		;;
 	1)
 		echo '<p>('"$d"') Gestern:'
@@ -26,6 +26,25 @@ touch $dir/index.new
 		;;
 	*)
 		echo "<p>($d) Vor $n Tagen:"
+		;;
+	esac
+	echo '<p><img src="'"day-$n.png"'">'
+	n=`expr $n + 1`
+done) >>$dir/archive.new
+mv -f $dir/archive.new $dir/archive.html
+
+n=0
+(for i in `cat data/flist`; do
+	# Needs gnu date, not darwin's date
+	# (and of course not solaris')
+	d="~ `TZ=GMT date -r $i.png +'%Y-%m-%d'`"
+	case "$n" in
+	0)
+		echo '<p>('"$d"') Heute:'
+		;;
+	*)
+		echo '<p><a href="archive.html">Vergangene Tage...</a>'
+		break;
 		;;
 	esac
 	echo '<p><img src="'"day-$n.png"'">'
